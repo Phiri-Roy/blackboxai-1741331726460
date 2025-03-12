f5c618634c3013be9ddc9ff2bf5101083aed721b
Phiri-H-Roy <107152452+Phiri-Roy@users.noreply.github.com>
2025-03-12 10:38:59 +0530

    Add sudoku_solver.py 1741756138940

---
+++ sudoku_solver.py
@@ -0,0 +1,13 @@
+def solve_sudoku(puzzle):
+    # Implement the backtracking algorithm to solve the Sudoku puzzle
+    def is_valid(board, row, col, num):
+        # Check if num can be placed at board[row][col]
+        pass
+
+    def backtrack(board):
+        # Backtracking algorithm implementation
+        pass
+
+    # Convert the puzzle into a 2D list and call backtrack
+    board = [[0]*9 for _ in range(9)]
+    return board  # Return the solved board
